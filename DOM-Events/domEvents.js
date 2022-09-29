16cc9c8c204bede10c9b9b5f6ffde0742ea3c68e
Filip Mateski <filipmateski13@gmail.com>
2022-09-29 13:49:58 +0200

    added dom events homework

--- DOM-Events/domEvents.js
+++ DOM-Events/domEvents.js
@@ -1,3 +1,4 @@
+1.
 function submitInput() {
 let fName = document.getElementById('fName').value;
 let lName = document.getElementById('lName').value;
@@ -15,14 +16,59 @@ let address = document.getElementById('address').value;
             alert("Empty field, please fill in.")
     }
 }
+// 2.
+const form = document.getElementById('form');
 
-function blur() {
-let fName = document.getElementById('fName').value;
-let lName = document.getElementById('lName').value;
-let email = document.getElementById('mail').value;
-let age = document.getElementById('age').value;
-let address = document.getElementById('address').value;
+function myF() {
+    let fName = document.getElementById('fName').value; 
+    let lName = document.getElementById('lName').value;
+    let email = document.getElementById('mail').value;
+    let age = document.getElementById('age').value;
+    let address = document.getElementById('address').value;
+
+    let fNameError = document.getElementById('errorfName');
+    let lNameError = document.getElementById('errorLName');
+    let errorMail = document.getElementById('errorMail');
+    let errorAge = document.getElementById('errorAge');
+    let errorAddress = document.getElementById('errorAddress');
     if(fName == "") {
-        document.querySelector(".errorfName").innerHTML = "Please Fill";
+        fNameError.innerHTML = "Input required";
+        return false;
+    }
+    if(lName == "") {
+        lNameError.innerHTML = "Input required";
+        return false;
+    }
+    if(email == "") {
+        errorMail.innerHTML = "Input required";
+        return false;
+    }
+    if(age == "") {
+        errorAge.innerHTML = "Input required";
+        return false;
+    }
+    if(address == "") {
+        errorAddress.innerHTML = "Input required";
+        return false;
     }
 }
+
+function key() {
+    document.getElementById('errorfName').innerHTML = "";
+    document.getElementById('errorLName').innerHTML = "";
+    document.getElementById('errorMail').innerHTML = "";
+    document.getElementById('errorAge').innerHTML = "";
+    document.getElementById('errorAddress').innerHTML = "";
+}
+// 3.
+let counter = 0;
+
+document.getElementById('fName').addEventListener("click", function () {
+  counter++;
+  console.log(counter);
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+  counter = 0;
+});
+console.log(counter);
